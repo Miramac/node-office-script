72ab760ec714b14ca88c4d3e8bc06de8597b7832
Miramac <miramac@gmx.net>
2015-11-27 17:46:08 +0100

    update package

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 var gulp = require('gulp');
 
 gulp.task('build', function() {
-
-  // Any deployment logic should go here
-   return gulp.src('./src/OfficeScript/OfficeScript/bin/Debug/*.dll')
-    .pipe(gulp.dest('./dist'));
- 
+    //Copy .NET functionsto /dist
+    return gulp.src('./src/OfficeScript/OfficeScript/bin/Debug/*.dll')
+    .pipe(gulp.dest('./dist'));´
 });
