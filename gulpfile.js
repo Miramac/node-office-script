15f68f8b9acf1d783dd2745f383dda603af210c3
seeg0r <seeg0r1337@gmail.com>
2015-12-22 16:15:45 +0100

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var exec = require('child_process').exec;
 var path = require('path');
 
-gulp.task('build', ['compile', 'deploy'], function() {
-    
+gulp.task('build', ['deploy'], function() {
+    console.log('Done!');
 });
 
 gulp.task('compile', function(cb) {
-    exec(`MSBuild ${path.normalize('src/OfficeScript/OfficeScript.sln')} /clp:ErrorsOnly /clp:ErrorsOnly`, function (err, stdout, stderr) {
+    exec(`MSBuild ${path.normalize('src/OfficeScript/OfficeScript.sln')} /clp:ErrorsOnly`, function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
@@ -15,7 +15,7 @@ gulp.task('compile', function(cb) {
 });
 
 
-gulp.task('deploy', function() {
+gulp.task('deploy', ['compile'], function() {
     
     //Copy .NET functionsto /dist
     return gulp.src('./src/OfficeScript/OfficeScript/bin/Debug/*.dll')
