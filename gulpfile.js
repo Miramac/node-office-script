784ad91d5f678a1d77dea597a54004690e224662
Fabian Roloff <fabian.roloff@vocatus.de>
2021-03-26 12:10:28 +0100

    fix msbuild path

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var dest = './dist'
 var src = './src/OfficeScript/OfficeScript/bin/Debug/*.dll'
 
 gulp.task('compile', function (cb) {
-  exec(`"c:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild" ${path.normalize('src/OfficeScript/OfficeScript.sln')} /clp:ErrorsOnly`, function (err, stdout, stderr) {
+  exec(`MSBuild ${path.normalize('src/OfficeScript/OfficeScript.sln')} /clp:ErrorsOnly`, function (err, stdout, stderr) {
     console.log(stdout)
     console.log(stderr)
     cb(err)
